b4c7e43c22853561ec6855111a661692caedd7af
oleksandr.navka <oleksandr.navka@globallogic.com>
2019-08-20 13:42:32 +0300

    added tests for uniqueElements task

--- src/test/java/com/globallogic/ArrayProcessingTest.java
+++ src/test/java/com/globallogic/ArrayProcessingTest.java
@@ -1,9 +1,34 @@
 package com.globallogic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ArrayProcessingTest {
 
     private ArrayProcessing unit = new ArrayProcessing();
 
+    @Test
+    void uniqueElementsArray() {
+        int[] actual = unit.uniqueElements(new int[]{1, 2, 4, 2, 5});
+        assertArrayEquals(new int[]{1, 2, 4, 5}, actual);
+    }
+
+    @Test
+    void uniqueElementsArrayWithAlreadyUniqueElements() {
+        int[] actual = unit.uniqueElements(new int[]{1, 2, 3, 4});
+        assertArrayEquals(new int[]{1, 2, 3, 4}, actual);
+    }
+
+    @Test
+    void uniqueElementsArrayWithSeveralUniqueElements() {
+        int[] actual = unit.uniqueElements(new int[]{1, 1, 2, 3, 2, 4, 2, 5});
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, actual);
+    }
+
+    @Test
+    void uniqueElementsArrayWithEmptyArray() {
+        int[] actual = unit.uniqueElements(new int[0]);
+        assertArrayEquals(new int[0], actual);
+    }
 }
