ad3f878826aaa1a6ff6e1f838a7b8dcd97edaece
oleksandr.navka <oleksandr.navka@globallogic.com>
2019-08-20 13:30:59 +0300

    added all tasks

--- src/main/java/com/globallogic/ArrayProcessing.java
+++ src/main/java/com/globallogic/ArrayProcessing.java
@@ -5,4 +5,20 @@ public class ArrayProcessing {
     public int[] uniqueElements(int[] arr) {
         return null;
     }
+
+    public int[] uniqueElementsMergeArray(int[] arr1, int[] arr2) {
+        return null;
+    }
+
+    public int[] commonElements(int[] arr1, int[] arr2) {
+        return null;
+    }
+
+    public int[] reverseArray(int[] arr) {
+        return null;
+    }
+
+    public int[] sortEvenPositionArray(int[] arr) {
+        return null;
+    }
 }
