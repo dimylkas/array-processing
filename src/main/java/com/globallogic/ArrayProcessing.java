59fab566488f838b369290337626a048f037ee1a
oleksandr.navka <oleksandr.navka@globallogic.com>
2019-08-20 17:13:51 +0300

    added uniqueElements solution

--- src/main/java/com/globallogic/ArrayProcessing.java
+++ src/main/java/com/globallogic/ArrayProcessing.java
@@ -1,9 +1,26 @@
 package com.globallogic;
 
+import java.util.Arrays;
+
 public class ArrayProcessing {
 
     public int[] uniqueElements(int[] arr) {
-        return null;
+        int[] resultArr = new int[arr.length];
+        int uniqueElementsCounter = 0;
+        for (int i : arr) {
+            boolean isElementUnique = true;
+            for (int j = 0; j < uniqueElementsCounter; j++) {
+                if (i == resultArr[j]) {
+                    isElementUnique = false;
+                    break;
+                }
+            }
+            if (isElementUnique) {
+                resultArr[uniqueElementsCounter] = i;
+                uniqueElementsCounter++;
+            }
+        }
+        return Arrays.copyOf(resultArr, uniqueElementsCounter);
     }
 
     public int[] uniqueElementsMergeArray(int[] arr1, int[] arr2) {
