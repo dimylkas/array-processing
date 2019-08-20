f2f3e64d01887c654836560fe14f04210e7fc08c
oleksandr.navka <oleksandr.navka@globallogic.com>
2019-08-20 17:42:11 +0300

    added reverseArray solution

--- src/main/java/com/globallogic/ArrayProcessing.java
+++ src/main/java/com/globallogic/ArrayProcessing.java
@@ -45,7 +45,12 @@ public class ArrayProcessing {
     }
 
     public int[] reverseArray(int[] arr) {
-        return null;
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+        return arr;
     }
 
     public int[] sortEvenPositionArray(int[] arr) {
