1bcb8e5cb888747d04e9bbbc1a0d40fb0b1fe747
oleksandr.navka <oleksandr.navka@globallogic.com>
2019-08-20 17:35:11 +0300

    added uniqueElementsMergeArray solution

--- src/main/java/com/globallogic/ArrayProcessing.java
+++ src/main/java/com/globallogic/ArrayProcessing.java
@@ -24,7 +24,10 @@ public class ArrayProcessing {
     }
 
     public int[] uniqueElementsMergeArray(int[] arr1, int[] arr2) {
-        return null;
+        int[] result = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, result, 0, arr1.length);
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+        return uniqueElements(result);
     }
 
     public int[] commonElements(int[] arr1, int[] arr2) {
